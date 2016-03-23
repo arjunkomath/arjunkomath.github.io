0fe98b612c0812206dcc17c5bb768d899232017b
Arjun Komath <arjunkomath@gmail.com>
2016-03-23 23:28:54 +0530

    update

--- app/data/projects.js
+++ app/data/projects.js
@@ -8,19 +8,19 @@ module.exports = [
 {
 	label: "Wush",
 	link: "https://wush.xyz/",
-	description: "Real-time features made easy. A web app powered by Node.js(sails) and socket.io",
+	description: "Easy to setup Push Notifications for any website. A web app powered by Node.js(sails) and socket.io",
 	image: "https://cdn.shopify.com/s/files/applications/c6fd2bbbccb0a7b75b4ee8d0918fd6f4.png"
 },
 {
 	label: "Techulus.xyz",
 	link: "http://techulus.xyz/",
-	description: "Website for shopping recomendationsWeb, Powered by Node.js (Express)",
+	description: "Website for shopping recomendations, Powered by Node.js (Express)",
 	image: "/img/techulus.png"
 },
 {
 	label: "Honest Reviews",
 	link: "https://play.google.com/store/apps/details?id=com.techulus.honestreviews&hl=en",
-	description: "Honest Reviews - Real Reviews by Real Users!",
+	description: "Honest Reviews - Real Reviews by Real Users! An android app for device reviews",
 	image: "https://lh3.ggpht.com/VKjtEkEUEHRhfwxh1mi5yUNKxrzlhztcsx1upFSKxNeI2cmg9HlLDpWl4M0Jyfn-YjI=w300-rw"
 }
 ];
